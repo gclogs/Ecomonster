7ca462bdd1f99472b686a521171e06c6c8110b40
GyeongChan <woig9874@naver.com>
2024-12-21 00:09:36 +0900

    feat: typescript에서 javascript로 transfile시 Script 문자열이 붙는 고충 해결

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -31,7 +31,9 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'ScriptApp': 'App'
+            'ScriptApp': 'App',
+            'ScriptMap': 'Map',
+            'ScriptPlayer': 'Player',
         })
     ],
     optimization: {
